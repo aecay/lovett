b05d0f658d6d183e18c58c01cd92e29a6ca81ad9
Aaron Ecay <aaronecay@gmail.com>
2015-10-05 03:21:45 +0100

    compat.py: add fn to strip cs comments

--- lovett/compat.py
+++ lovett/compat.py
@@ -1,2 +1,21 @@
 def strip_corpussearch_comments(s):
-    pass
+    output = []
+    comment = False
+    for line in s.split("\n"):
+        if line.startswith("/*") or line.startswith("/~*"):
+            comment = True
+        elif line.startswith("<+"):
+            # Ignore parser-mode comments
+            pass
+        elif not comment:
+            output.append(line)
+        elif line.startswith("*/") or line.startswith("*~/"):
+            comment = False
+        else:  # pragma: no cover
+            # Should never happen!
+            pass
+
+    if comment:
+        raise Exception("Unterminated comment in input file!")
+
+    return "\n".join(output)
