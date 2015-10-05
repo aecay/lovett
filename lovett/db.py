93b6acdaa3c2b2369e9c5d80d5683dc1e193627f
Aaron Ecay <aaronecay@gmail.com>
2015-10-05 03:25:51 +0100

    db.py: use sqlalchemy

--- lovett/db.py
+++ lovett/db.py
@@ -1,4 +1,6 @@
-import apsw
+import sqlalchemy
+from sqlalchemy import Table, Column, Integer, String, ForeignKey, MetaData
+from sqlalchemy.sql import select
 
 import lovett.util as util
 import lovett.corpus as corpus
@@ -7,45 +9,55 @@ import lovett.tree as tree
 
 class CorpusDb(corpus.CorpusBase):
     def __init__(self):
-        self.conn = apsw.Connection(":memory:")
-        c = self.conn.cursor()
-        c.execute("PRAGMA foreign_keys = ON")
-        c.execute("CREATE TABLE nodes (rowid INTEGER PRIMARY KEY, label STRING)")
-        c.execute("""CREATE TABLE dom (parent INT, child INT, depth INT,
-                                       FOREIGN KEY(parent) REFERENCES nodes(rowid),
-                                       FOREIGN KEY(child) REFERENCES nodes(rowid))""")
-        c.execute("""CREATE TABLE sprec (left INT, right INT, distance INT,
-                                         FOREIGN KEY(left) REFERENCES nodes(rowid),
-                                         FOREIGN KEY(right) REFERENCES nodes(rowid))""")
-        c.execute("CREATE TABLE roots (id INT, FOREIGN KEY(id) REFERENCES nodes(rowid))")
-        c.execute("""CREATE TABLE metadata (id INT, key STRING, value STRING,
-                                            FOREIGN KEY(id) REFERENCES nodes(rowid))""")
-        # TODO: need triggers, or to break out add_child, add_sibling, and
-        # remove_ variants
+        self.engine = sqlalchemy.create_engine("sqlite:///:memory:")
+        self.metadata = MetaData()
+        self.nodes = Table("nodes", self.metadata,
+                           Column("rowid", Integer, primary_key=True),
+                           Column("label", String))
+        self.dom = Table("dom", self.metadata,
+                         Column("parent", Integer, ForeignKey("nodes.rowid")),
+                         Column("child", Integer, ForeignKey("nodes.rowid")),
+                         Column("depth", Integer))
+        self.sprec = Table("sprec", self.metadata,
+                           Column("left", Integer, ForeignKey("nodes.rowid")),
+                           Column("right", Integer, ForeignKey("nodes.rowid")),
+                           Column("distance", Integer))
+        self.roots = Table("roots", self.metadata,
+                           Column("id", Integer, ForeignKey("nodes.rowid")))
+        self.tree_metadata = Table("metadata", self.metadata,
+                                   Column("id", Integer, ForeignKey("nodes.rowid")),
+                                   Column("key", String),
+                                   Column("value", String))
+        self.metadata.create_all(self.engine)
 
+    # TODO: use triggers instead of _add_child and _add_sibling
     def _add_child(self, parent, child):
-        c = self.conn.cursor()
-        c.execute("""INSERT INTO dom (parent, child, depth)
-                     SELECT p.parent, c.child, p.depth+c.depth+1
-                     FROM dom p, dom c
-                     WHERE p.child = ? AND c.parent = ?""", (parent, child))
+        c = self.engine.connect()
+        update = sqlalchemy.sql.text(
+            """INSERT INTO dom (parent, child, depth)
+            SELECT p.parent, c.child, p.depth+c.depth+1
+            FROM dom p, dom c
+            WHERE p.child = :parent AND c.parent = :child""")
+        c.execute(update, parent=parent, child=child)
 
     def _add_sibling(self, left, right):
-        c = self.conn.cursor()
-        c.execute("""INSERT INTO sprec (left, right, distance)
-                     SELECT l.left, r.right, l.distance+r.distance+1
-                     FROM sprec l, sprec r
-                     WHERE l.right = ? AND r.left = ?""", (left, right))
+        c = self.engine.connect()
+        update = sqlalchemy.sql.text(
+            """INSERT INTO sprec (left, right, distance)
+            SELECT l.left, r.right, l.distance+r.distance+1
+            FROM sprec l, sprec r
+            WHERE l.right = :left AND r.left = :right""")
+        c.execute(update, left=left, right=right)
 
     def _insert_node(self, node, parent=None, left=None):
-        c = self.conn.cursor()
+        c = self.engine.connect()
         # Insert this node's label into the db
-        c.execute("INSERT INTO nodes VALUES (NULL, ?)", (node.label,))
-        rowid = c.execute("SELECT last_insert_rowid()").fetchone()[0]
+        c.execute(self.nodes.insert(), label=node.label)
+        rowid = c.execute(sqlalchemy.sql.text("SELECT last_insert_rowid()")).fetchone()[0]
         # Add it to the dominance_R table
-        c.execute("INSERT INTO dom VALUES (?,?,0)", (rowid, rowid))
+        c.execute(self.dom.insert(), parent=rowid, child=rowid, depth=0)
         # Add it to the sprecedes_R table
-        c.execute("INSERT INTO sprec VALUES (?,?,0)", (rowid, rowid))
+        c.execute(self.sprec.insert(), left=rowid, right=rowid, distance=0)
         # Make it a child of its parent, if applicable
         if parent:
             self._add_child(parent, rowid)
@@ -53,7 +65,7 @@ class CorpusDb(corpus.CorpusBase):
             self._add_sibling(left, rowid)
         # Add its text to the metadata db...
         if util.is_leaf(node):
-            c.execute("INSERT INTO metadata VALUES (?,'text',?)", (rowid, node.text))
+            c.execute(self.tree_metadata.insert(), id=rowid, key="text", value=node.text)
         # ...or add its children to the db, as applicable
         if util.is_nonterminal(node):
             lastchild_rowid = None
@@ -63,35 +75,44 @@ class CorpusDb(corpus.CorpusBase):
 
     def insert_tree(self, t):
         rowid = self._insert_node(t)
-        c = self.conn.cursor()
-        c.execute("INSERT INTO roots VALUES (?)", (rowid,))
+        c = self.engine.connect()
+        c.execute(self.roots.insert(), id=rowid)
 
     def _reconstitute(self, rowid):
         # TODO: handle metadata
-        c = self.conn.cursor()
-        label = c.execute("SELECT label FROM nodes WHERE rowid = ?", (rowid,)).fetchone()[0]
+        c = self.engine.connect()
+        label = c.execute(
+            select([self.nodes.c.label]).where(self.nodes.c.rowid == rowid)
+        ).fetchone()[0]
         # TODO: possibly bogus optimization assuming that nodes are always
         # inserted into the DB in order.  If we allow mutable databases, this
         # will need to be changed.
-        children = c.execute("SELECT child FROM dom WHERE depth = 1 AND parent = ? ORDER BY child",
-                             (rowid,)).fetchall()
+        children = c.execute(
+            select([self.dom.c.child]).
+            where((self.dom.c.depth == 1) & (self.dom.c.parent == rowid)).
+            order_by(self.dom.c.child)
+        ).fetchall()
         if len(children) > 0:
             child_trees = [self._reconstitute(child[0]) for child in children]
             return tree.NonTerminal(label, child_trees)
         else:
-            text = c.execute("SELECT value FROM metadata WHERE id = ? AND key = 'text'",
-                             (rowid,)).fetchone()[0]
+            text = c.execute(
+                select([self.tree_metadata.c.value]).
+                where((self.tree_metadata.c.id == rowid) & (self.tree_metadata.c.key == "text"))
+            ).fetchone()[0]
             return tree.Leaf(label, text)
 
     # Corpus abstract methods
     def __getitem__(self, i):
-        c = self.conn.cursor()
+        c = self.engine.connect()
         return self._reconstitute(
-            c.execute("SELECT rowid FROM roots ORDER BY rowid").fetchall()[i][0])
+            c.execute(
+                sqlalchemy.sql.text("SELECT rowid FROM roots ORDER BY rowid")
+            ).fetchall()[i][0])
 
     def __len__(self):
         c = self.conn.cursor()
-        return len(c.execute("SELECT * FROM roots").fetchall())
+        return len(c.execute(sqlalchemy.sql.text("SELECT * FROM roots")).fetchall())
 
     # Instance methods
 
