80b479871991549986a0aff4f253bb11a5065b07
Aaron Ecay <aaronecay@gmail.com>
2015-10-09 15:28:22 +0100

    transform.py: implement more transforms

--- lovett/transform.py
+++ lovett/transform.py
@@ -13,6 +13,14 @@ The following corpora are supported by the following functions:
   * `icepahc_lemma`
   * `icepahc_year`
 
+* The PPCHE (in general)
+
+  * `ppche_make_unicode`
+
+* The YCOE
+
+  * `ycoe_case`
+
 * General utility
 
   * `ensure_id`
@@ -22,13 +30,13 @@ The following corpora are supported by the following functions:
    To support:
 
    * IcePaHC: icepahc_word_splits
-   * PPCHE (generally): ppche_make_unicode, ppche_word_splits
-   * YCOE: ycoe_case
+   * PPCHE (generally): ppche_word_splits
    * PCEEC: pceec_metadata
 
 """
 
 from hashlib import md5
+import re
 
 import lovett.util as util
 
@@ -56,6 +64,12 @@ def _icepahc_case_do(tree):
 
 
 def icepahc_case(tree):
+    """Convert IcePaHC case to metadata.
+
+    In IcePaHC, case is indicated as one of four dash tags attached to
+    case-bearing lexical categories.
+
+    """
     _icepahc_case_do(tree)
     if util.is_nonterminal(tree):
         for subtree in tree:
@@ -63,6 +77,11 @@ def icepahc_case(tree):
 
 
 def icepahc_lemma(tree):
+    """Convert IcePaHC lemmata to matadata.
+
+    In IcePaHC, lemmata are stored in the leaf text as ``text-lemma``.
+
+    """
     if util.is_nonterminal(tree):
         for subtree in tree:
             icepahc_lemma(subtree)
@@ -81,6 +100,119 @@ def icepahc_year(tree):
 
     """
     tree.metadata.year = int(tree.metadata.file[0:4])
+
+#: Mapping between PPCHE codes and their unicode translation.  See section
+#: B.2.2 at http://clu.uni.no/icame/manuals/HC/INDEX.HTM#con31
+PPCHE_UNICODE_TRANSFORMS = [
+    ("+a", "æ"),
+    ("+A", "Æ"),
+    ("+t", "þ"),
+    ("+T", "Þ"),
+    ("+d", "ð"),
+    ("+D", "Ð"),
+    ("+g", "ʒ"),
+    ("+G", "Ʒ"),
+    ("+L", "£"),
+    ("+e", "ę"),
+    ("+tt", "ꝥ"),
+    ("+TT", "Ꝥ"),
+    ("+Tt", "Ꝥ")
+]
+
+#: Mapping between letters and their Unicode superscripts
+PPCHE_UNICODE_SUPERSCRIPT = {
+    "a": "ᵃ",
+    "b": "ᵇ",
+    "c": "ᶜ",
+    "d": "ᵈ",
+    "e": "ᵉ",
+    "f": "ᶠ",
+    "g": "ᵍ",
+    "h": "ʰ",
+    "i": "ⁱ",
+    "j": "ʲ",
+    "k": "ᵏ",
+    "l": "ˡ",
+    "m": "ᵐ",
+    "n": "ⁿ",
+    "o": "ᵒ",
+    "p": "ᵖ",
+    # No superscript q!
+    "r": "ʳ",
+    "s": "ˢ",
+    "t": "ᵗ",
+    "u": "ᵘ",
+    "v": "ᵛ",
+    "w": "ʷ",
+    "x": "ˣ",
+    "y": "ʸ",
+    "z": "ᶻ"
+}
+
+
+def _ppche_make_unicode_superscript(matchobj):
+    g = matchobj.group(1)
+    try:
+        return "".join(map(lambda x: PPCHE_UNICODE_SUPERSCRIPT[x], list(g)))
+    except KeyError:
+        raise Exception("Could not convert to superscript: " + g)
+
+
+def _ppche_make_unicode_leaf(leaf):
+    for t in PPCHE_UNICODE_TRANSFORMS:
+        leaf.text = leaf.text.replace(t[0], t[1])
+    leaf.text = re.sub("=(.*?)=", _ppche_make_unicode_superscript, leaf.text)
+
+
+def ppche_make_unicode(tree):
+    """Convert characters in PPCHE format to unicode.
+
+    Currently handles the ``+X`` notation for aesc, thorn, etc., as well as
+    partial support for superscripts delimited by ``=...=``.
+
+    """
+    for child in tree:
+        if util.is_leaf(tree):
+            _ppche_make_unicode_leaf(child)
+        else:
+            ppche_make_unicode(child)
+
+
+#: A mapping between caret-tags and case names in the YCOE
+YCOE_CASES = {
+    "A": "accusative",
+    "N": "nominative",
+    "D": "dative",
+    "G": "genitive",
+    "I": "instrumental"
+}
+
+#: A mapping between caret-tags and addverb types in the YCOE
+YCOE_ADVERB_TYPES = {
+    "T": "temporal",
+    "L": "locative"
+}
+
+
+def ycoe_case(tree):
+    """Convert case in the YCOE into metadata.
+
+    Also handles the adverb-type notations ``^L`` and ``^T`` (locative and
+    temporal), which the YCOE codes with a notation like that for case.
+
+    """
+    l = tree.label.split("^")
+    if len(l) > 1:
+        if len(l) != 2:
+            raise Exception("Label with more than 1 '^': %s" % tree.label)
+        tree.label = l[0]
+        if l[0] == "ADV":
+            tree.metadata.adverb_type = YCOE_ADVERB_TYPES[l[1]]
+        else:
+            tree.metadata.case = YCOE_CASES[l[1]]
+    if not util.is_leaf(tree):
+        for child in tree:
+            ycoe_case(child)
 
 
 def ensure_id(tree):
