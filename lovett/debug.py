e90920653dbd66d4f2087b3e87b120a948f08964
Aaron Ecay <aaronecay@gmail.com>
2015-12-09 17:34:52 +0000

    debug: add xqp_sa function

--- lovett/debug.py
+++ lovett/debug.py
@@ -4,9 +4,18 @@ import io
 import pstats
 import contextlib
 
+
 def xqp(corpusdb, query_text):
     c = corpusdb.engine.connect()
     return c.execute(sqlalchemy.sql.text("EXPLAIN QUERY PLAN " + query_text)).fetchall()
+
+
+def xqp_sa(corpusdb, query_obj):
+    c = corpusdb.engine.connect()
+    return c.execute(sqlalchemy.sql.text("EXPLAIN QUERY PLAN " +
+                                         str(query_obj.compile(corpusdb.engine,
+                                                               compile_kwargs={"literal_binds": True})))
+    ).fetchall()
 
 
 @contextlib.contextmanager
