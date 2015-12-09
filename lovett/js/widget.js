e85b2d635c00283d7112935dce67a438cf348233
Aaron Ecay <aaronecay@gmail.com>
2015-12-09 17:17:47 +0000

    wdigets: new ResultsView widget

--- lovett/js/widget.js
+++ lovett/js/widget.js
@@ -6,26 +6,34 @@ require(
     function(widget, manager) {
         // Define the view
         var TreesView = widget.DOMWidgetView.extend({
+            initialHtml: "<div class=\"treeview-header\">" +
+                "<span class=\"treeview-info\">" +
+                "<span class=\"treeview-index\"></span>" +
+                "/" +
+                "<span class=\"treeview-maxindex\"></span>" +
+                "</span>" +
+                "<span class=\"treeview-buttons\">" +
+                "<button type=\"button\" class=\"back-btn\">&lt;</input>" +
+                "<button type=\"button\" class=\"fwd-btn\">&gt;</input>" +
+                "</span>" +
+                "</div>" +
+                "<div class=\"tree-display\" id=\"tree-display\"></div>",
+
             render: function () {
                 var el = this.$el;
                 var that = this;
-                el.html("<div class=\"treeview-header\">" +
-                        "<span class=\"treeview-info\">" +
-                        "<span class=\"treeview-index\"></span>" +
-                        "/" +
-                        "<span class=\"treeview-maxindex\"></span>" +
-                        "</span>" +
-                        "<span class=\"treeview-buttons\">" +
-                        "<button type=\"button\" class=\"back-btn\">&lt;</input>" +
-                        "<button type=\"button\" class=\"fwd-btn\">&gt;</input>" +
-                        "</span>" +
-                        "</div>" +
-                        "<div class=\"tree-display\" id=\"tree-display\"></div>");
+                el.html(this.initialHtml);
                 el.find(".back-btn").click(function () { that.back(); });
                 el.find(".fwd-btn").click(function () { that.fwd(); });
                 el.find(".treeview-maxindex").text(this.model.get("maxindex") + 1);
                 this.model.on("change:index", function () { that.showTree(); });
-                that.showTree();
+                this.model.on("change:html", function () { that.htmlChange(); });
+                this.initializeView();
+            },
+
+            initializeView: function () {
+                this.showTree();
+
             },
 
             back: function () {
@@ -40,15 +48,43 @@ require(
             },
 
             showTree: function () {
-                this.$el.find(".treeview-index").text(this.model.get("index") + 1);
-                this.$el.find("#tree-display").html(
-                    this.model.get("trees")[this.model.get("index")]);
+                var idx = this.model.get("index");
+                this.$el.find(".treeview-index").text(idx + 1);
+                this.send({ action: "get-tree", index: idx });
+                // this.$el.find("#tree-display").html(
+                //     this.model.get("trees")[this.model.get("index")]);
+            },
+
+            htmlChange: function () {
+                this.$el.find("#tree-display").html(this.model.get("html"));
             }
 
-            // TODO: should be able to use this.send(content) to send messages
-            // to python.  corresponiding python function is Widget.on_msg
+        });
+
+        var ResultsView = TreesView.extend({
+            initialHtml: "<div class=\"treeview-header\">" +
+                "<div class=\"treeview-query\">Results for query " +
+                "<span class=\"treeview-query-html\"></span>" +
+                "</div>" +
+                "<span class=\"treeview-info\">" +
+                "<span class=\"treeview-index\"></span>" +
+                "/" +
+                "<span class=\"treeview-maxindex\"></span>" +
+                "</span>" +
+                "<span class=\"treeview-buttons\">" +
+                "<button type=\"button\" class=\"back-btn\">&lt;</input>" +
+                "<button type=\"button\" class=\"fwd-btn\">&gt;</input>" +
+                "</span>" +
+                "</div>" +
+                "<div class=\"tree-display\" id=\"tree-display\"></div>",
+
+            initializeView: function () {
+                this.$el.find(".treeview-query-html").html(this.model.get("query_html"));
+                this.showTree();
+            }
         });
 
         // Register the view with the widget manager.
         manager.WidgetManager.register_widget_view('TreesView', TreesView);
+        manager.WidgetManager.register_widget_view('ResultsView', ResultsView);
 });
--- lovett/widgets.py
+++ lovett/widgets.py
@@ -1,18 +1,52 @@
-import ipywidgets as widgets
-from traitlets import Unicode, List, Int
+import ipywidgets
+from traitlets import Unicode, Int
+
+# TODO: why does running a cell with a ResultsView result twice advance the
+# tree viewer?
 
 
-class TreesView(widgets.DOMWidget):
+def get_tree(widget, content, buffers):
+    if "action" in content and content["action"] == "get-tree":
+        widget.html = widget.trees[content["index"]]._repr_html_()
+
+
+class TreesView(ipywidgets.DOMWidget):
     _view_name = Unicode("TreesView", sync=True)
 
-    trees = List(Unicode, sync=True)
     index = Int(sync=True)
     # TODO: Is there a good way to say this is read-only (or more accurately
     # write-once)?
     maxindex = Int(sync=True)
+    html = Unicode("", sync=True)
 
     def __init__(self, trees):
         super().__init__()
         self.trees = trees
         self.index = 0
         self.maxindex = len(trees) - 1
+        self.begin()
+
+    def begin(self):
+        self.on_msg(get_tree)
+
+
+def get_tree_resultsview(widget, content, buffers):
+    if "action" in content and content["action"] == "get-tree":
+        tree = widget.trees[content["index"]]
+        widget._query.colorize_tree(tree)
+        widget.html = tree._repr_html_()
+
+
+class ResultsView(TreesView):
+    _view_name = Unicode("ResultsView", sync=True)
+    query_html = Unicode(sync=True)
+
+    def __init__(self, trees, query):
+        # TODO: should we make a copy of the query here?
+        self._query = query
+        self._query.colorize()
+        self.query_html = self._query._repr_html_()
+        super().__init__(trees)
+
+    def begin(self):
+        self.on_msg(get_tree_resultsview)
