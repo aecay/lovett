6c1a1e58a28e63b00d363c03358c96860e9b9043
Aaron Ecay <aaronecay@gmail.com>
2015-12-09 17:35:10 +0000

    ilovett.py: add %%tree magic

--- lovett/ilovett.py
+++ lovett/ilovett.py
@@ -1,10 +1,35 @@
 """Functions for use in the IPython notebook interface."""
 
 from IPython.display import display, HTML
+from IPython.core.magic import (Magics, magics_class, cell_magic)
 import pkg_resources
+import lovett.tree as tree
+import keyword
+
+
+injected = False
+
+
+@magics_class
+class LovettMagics(Magics):
+    @cell_magic
+    def tree(self, line, cell):
+        # TODO: are we sure we like this method of assigning the tree to a
+        # variable?
+        t = tree.parse(cell)
+        if line is not None and line.isidentifier() and not keyword.iskeyword(line):
+            self.shell.user_ns[line] = t
+        return t
 
 
 def ilovett():
+    global injected
+    injected = True
+    try:
+        ip = get_ipython()
+        ip.register_magics(LovettMagics)
+    except:
+        pass
     return display(HTML("""<script>$("head").append($("<style type='text/css' />").text('%s'));</script>""" %
                         open(pkg_resources.resource_filename("lovett", "css/ipython.css")).read().replace("\n", "\\n") +
                         "<script>%s</script>" % open(pkg_resources.resource_filename("lovett", "js/widget.js")).read()))
